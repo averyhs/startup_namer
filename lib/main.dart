c223fb1061618864960d87027d727457992d76ee
click-to-generate-name <avery2718.jpi@gmail.com>
2021-07-12 16:36:39 +0200

    Add list functionality
    Expand RandomWordsState to generate an infinitely scrollable list of
    random words

--- lib/main.dart
+++ lib/main.dart
@@ -31,10 +31,51 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18); // option for larger font
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold (
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        // The itemBuilder callback is called once per suggested word pairing,
+        // and places each suggestion into a ListTile row. For even rows, the
+        // function adds a ListTile row for the word pairing. For odd rows, the
+        // function adds a Divider widget to visually separate the entries. Note
+        // that the divider may be difficult to see on smaller devices.
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            // Add 1 pixel high divider widget before each row in ListView.
+            return Divider();
+          }
+
+          // The syntax "i ~/ 2" divides i by 2 and returns an integer result.
+          // This calculates the actual number of word pairings in the ListView,
+          // minus the divider widgets.
+          final int index = i ~/ 2;
+          if (index >= _suggestions.length) { // end of list
+            // Generate 10 more suggestions
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
-
